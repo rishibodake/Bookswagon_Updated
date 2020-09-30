5d77c042ef24b361820fe9aada0d18452b6b6c6e
rishibodake <rishibodake@gmail.com>
2020-09-30 18:15:21 +0530

    Added:Test Class With Base Class

---
+++ Bookswagon_Updated/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Bookswagon_Updated
+{
+    public class BaseClass
+    {
+        public IWebDriver driver;
+        [OneTimeSetUp]
+        public void Initialise()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("-start-maximized");
+            driver = new ChromeDriver(chromeOptions);
+            driver.Url = "https://bookswagon.com";
+        }
+    }
+}
---
+++ Bookswagon_Updated/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Bookswagon_Updated
+{
+    [TestFixture]
+    public class TestClass : BaseClass
+    {     
+        [Test]
+        public void Test1()
+        {
+
+        }
+        [OneTimeTearDown]
+        public void CloseUp()
+        {
+            driver.Quit();
+        }
+    }
+}
